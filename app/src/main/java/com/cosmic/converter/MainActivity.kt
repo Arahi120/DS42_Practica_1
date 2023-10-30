672f256f4d4de8b0e0626a9b93c2a6f439f2c418
Arahi120 <a22311144@uthermosillo.edu.mx>
2023-10-30 10:24:15 -0700

    se agregaron las formulas de la temperatura

--- app/src/main/java/com/cosmic/converter/MainActivity.kt
+++ app/src/main/java/com/cosmic/converter/MainActivity.kt
@@ -3,21 +3,29 @@ package com.cosmic.converter
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private  var conversion : Int = 0
+    private var resul: Double=0.0
+    private var value: String = ""
+    private lateinit var result : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var selectedTemp = findViewById<Spinner>(R.id.temp_options)
-        var temp= findViewById<EditText>(R.id.temperatura)
-        var button= findViewById<Button>(R.id.convertir)
-        var result= findViewById<TextView>(R.id.resultado)
+        val selectedTemp = findViewById<Spinner>(R.id.temp_options)
+        val temp= findViewById<EditText>(R.id.temperatura)
+        val button= findViewById<Button>(R.id.convertir)
+        result = findViewById(R.id.resultado)
+
 
         if(selectedTemp != null){
             val adapter= ArrayAdapter(
@@ -26,16 +34,67 @@ class MainActivity : AppCompatActivity() {
                 resources.getStringArray(R.array.convert_options)
             )
             selectedTemp.adapter=adapter
+
+            selectedTemp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    Toast.makeText(this@MainActivity, "Item seleccionado: $position", Toast.LENGTH_SHORT).show()
+                    conversion = position
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    // Código para cuando no se selecciona ningún elemento
+                }
+            }
+
         }
 
-        button.setOnClickListener(
+        button.setOnClickListener {
 
-            temp.text
-        )
+            value = temp.text.toString()
+            covert(conversion)
+
+
+        }
 
     }
-    fun covert() {
-        //TODO crear las conversiones
-        //Tarea: Traer las formulas de conversion
+    fun covert(_conversion: Int) {
+        when(_conversion) {
+            0 -> {
+                // F = (C * 9/5) + 32
+                resul= (value.toDouble() * 9/5 ) +32
+                result.text= resul.toString()
+            }
+            1 -> {
+                // K= C + 273.15
+                resul = (value.toDouble()+ 273.15)
+                result.text=resul.toString()
+            }
+            2 -> {
+                // C = (F - 32) * 5/9
+                resul= (value.toDouble() - 32) *5/9
+                result.text=resul.toString()
+            }
+            3 -> {
+                // (F - 32) * 5/9 + 273.15
+                resul=(value.toDouble() -32) *5/9 + 273.15
+                result.text=resul.toString()
+            }
+            4 -> {
+                //C = K - 273.15
+                resul=(value.toDouble()-273.15)
+                result.text=resul.toString()
+            }
+            5 -> {
+                //F = (K - 273.15) * 1.8
+                resul = (value.toDouble() - 273.15) * 1.8 + 32
+                result.text=resul.toString()
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/cosmic/converter/models/Temperatura.kt
@@ -0,0 +1,7 @@
+package com.cosmic.converter.models
+
+data class Temperature(
+    val temperature: Double,
+    val origin: Char,
+    val destiny: Char,
+)
